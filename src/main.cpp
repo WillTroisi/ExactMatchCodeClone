8c559bbd75e19ed689c97606d944381c7a888831
WillTroisi <wmtroisi@loyola.edu>
2026-04-14 17:06:23 -0400

    Organized output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
-#include <detector.hpp>
+#include <fstream>
+#include <vector>
+
+#include "normalize.hpp"
 
 int main() {
+    std::ifstream file("test.cpp");
+
+    if (!file) {
+        std::cerr << "Failed to open test.cpp\n";
+        return 1;
+    }
+
+    std::vector<std::string> words = normalize(file);
+
+    std::cout << std::endl;
+    for (const auto& word : words) {
+        std::cout << word << "\n";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
